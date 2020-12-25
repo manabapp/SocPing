25c49d7cb2ad83aef8840184973f1ded9fccd812
manabapp <manabapp>
2020-12-25 12:09:51 +0900

    Initial Commit

---
+++ SocPing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocPing
+//
+//  Created by Hirose Manabu on 2020/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
